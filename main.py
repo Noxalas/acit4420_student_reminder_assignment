94c69d57bd0c8712c723c29d53452610fcb88b8a
Noxalas <natanielwlo@gmail.com>
2025-10-13 13:38:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,2 @@
+# Develop a script that integrates all modules to perform the automation process: manage student data, generate personalized reminders, simulate sending, log operations, and schedule daily execution
+# Simulate daily automation using the scheduler.py module, allowing manual triggering for testing.
---
+++ modules/students.py
@@ -0,0 +1,11 @@
+class Students:
+    def __init__(self):
+        self.students = []
+
+    def add_students():
+        """"""
+        student = {
+                ""
+                }
+
+        self.students
